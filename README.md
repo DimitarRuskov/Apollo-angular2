63dc513789e8e94b7e842fc3cbe912fa459e1d0a
DimitarRuskov <rawket95@gmail.com>
2016-03-26 09:49:36 +0200

    dev: added root component

---
+++ README.md
@@ -0,0 +1,5 @@
+-Steps to set up tslint:
+1. install VSCode tslint extension
+2. npm install -g tslint
+...
+
---
+++ app/root.component.ts
@@ -0,0 +1,8 @@
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'im-root',
+     template: '<h1>Init</h1>'
+})
+
+export class RootComponent { }
