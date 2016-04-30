12d7f0eb50b7f305f6fb46d09791e679b4fb28ba
DimitarRuskov <dimi7ar.ru5kov@gmail.com>
2016-05-01 00:42:28 +0300

    dev: added sample directive for future reference

--- app/components/home/home.component.ts
+++ app/components/home/home.component.ts
@@ -1,7 +1,9 @@
 import {Component} from 'angular2/core';
+import {SampleDirective} from '../../directives/sample.directive'
 
 @Component({
   templateUrl:  'app/components/home/home.component.html',
-  styleUrls:    ['app/components/home/home.component.css']
+  styleUrls:    ['app/components/home/home.component.css'],
+  directives:   [SampleDirective]
 })
 export class HomeComponent { }
---
+++ app/directives/sample.directive.ts
@@ -0,0 +1,19 @@
+import {Directive, ElementRef, OnInit, Renderer} from 'angular2/core';
+
+@Directive({
+    selector: '[apolloSample]',
+    host: {
+        '(mouseenter)': 'onMouseEnter()',
+        '(mouseleave)': 'onMouseLeave()'
+    }
+})
+
+export class SampleDirective implements OnInit {
+    private _defaultColor = 'green';
+    
+    constructor(private _elRef:ElementRef, private _renderer:Renderer) {}
+    
+    ngOnInit() {
+        this._renderer.setElementStyle(this._elRef.nativeElement, 'color', this._defaultColor)
+    }
+}
