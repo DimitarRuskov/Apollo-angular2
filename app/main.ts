8f8c3321ea91af26d6260608b555b2c64a9e46d8
DimitarRuskov <dimi7ar.ru5kov@gmail.com>
2016-05-05 00:10:37 +0300

    dev: removed transaltion module stuff

--- app/main.ts
+++ app/main.ts
@@ -4,7 +4,6 @@ import {ROUTER_PROVIDERS}   from 'angular2/router';
 import {HTTP_PROVIDERS}     from 'angular2/http';
 
 import {RootComponent}      from './root.component';
-import {TranslatePipe}      from './pipes/translate.pipe'
 import {NotificationsService} from '../node_modules/angular2-notifications/components';
 
-bootstrap(RootComponent, [HTTP_PROVIDERS, ROUTER_PROVIDERS, NotificationsService, [provide(PLATFORM_PIPES, {useValue: [TranslatePipe], multi: true})]]);
+bootstrap(RootComponent, [HTTP_PROVIDERS, ROUTER_PROVIDERS, NotificationsService]);
