75fae2cb74d43e23d0d4252a89c0b2a1764189c6
Deyvid Raychev <Deyvid.Raychev@Softwaregroup-bg.com>
2016-05-13 10:59:45 +0300

    dev nnavbar

--- app/components/home/home.component.ts
+++ app/components/home/home.component.ts
@@ -1,10 +1,9 @@
 import {Component} from '@angular/core';
-import {SampleDirective} from 'shared/directives/sample.directive';
 import {MaterializeDirective} from 'angular2-materialize';
 
 @Component({
   templateUrl:  'app/components/home/home.component.html',
   styleUrls:    ['app/components/home/home.component.css'],
-  directives:   [SampleDirective, MaterializeDirective]
+  directives:   [MaterializeDirective]
 })
 export class HomeComponent { }
