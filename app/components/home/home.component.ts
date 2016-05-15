6ed4c6b575c9f7205634d12c334a6d314593439f
DimitarRuskov <dimi7ar.ru5kov@gmail.com>
2016-05-16 01:56:55 +0300

    dev: WIP slider

--- app/components/home/home.component.ts
+++ app/components/home/home.component.ts
@@ -1,8 +1,9 @@
-import {Component} from '@angular/core';
+import {Component}        from '@angular/core';
+import {SliderComponent}  from 'shared/components/slider/slider.component';
 
 @Component({
   templateUrl:  'app/components/home/home.component.html',
   styleUrls:    ['app/components/home/home.component.css'],
-  directives:   []
+  directives:   [SliderComponent]
 })
 export class HomeComponent { }
