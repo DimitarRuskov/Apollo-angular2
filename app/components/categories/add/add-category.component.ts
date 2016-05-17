de4adad61c7c4cb14310f463bb5d91d3a29c0e50
DimitarRuskov <dimi7ar.ru5kov@gmail.com>
2016-05-18 02:43:18 +0300

    dev: design improvements

--- app/components/categories/add/add-category.component.ts
+++ app/components/categories/add/add-category.component.ts
@@ -1,18 +1,19 @@
-import {Component, ElementRef} from '@angular/core';
-import {CategoryService} from './../category.service';
-import {AuthService} from 'shared/services/auth.service';
+import {Component, ElementRef}  from '@angular/core';
+import {CategoryService}        from './../category.service';
+import {AuthService}            from 'shared/services/auth.service';
+import {UtilsService}           from 'shared/services/utils.service';
 
 @Component({
     templateUrl:        'app/components/categories/add/add-category.component.html',
     styleUrls:          ['app/components/categories/add/add-category.component.css'],
-    providers:          [CategoryService]
+    providers:          [CategoryService, UtilsService]
 })
 
 export class AddCategoryComponent {
     private _selectedImage: any = null;
-    constructor(private element: ElementRef, private _categoryService: CategoryService, private _authService: AuthService) { }
+    constructor(private element: ElementRef, private _categoryService: CategoryService, private _authService: AuthService, private _utilsService: UtilsService) { }
     
-    changeListner(event: any) {
+    changeListener(event: any) {
         let reader = new FileReader();
         let image = this.element.nativeElement.querySelector('.image');
 
@@ -27,7 +28,18 @@ export class AddCategoryComponent {
     
     onSubmit(values: any): void {
         values.image = this._selectedImage;
-        this._categoryService.addCategory(values);
+        this._categoryService.addCategory(values)
+        .subscribe(
+            (data: any) => {
+                
+            },
+            (error: any) => {
+                
+            },
+            () => {
+                this._utilsService.success('Logged in');
+            }
+        );
     }
     
     canActivate(next: any, prev: any) {
