51faad4ccb8687be2b09b9f67cbf7e850b24e79a
DimitarRuskov <dimi7ar.ru5kov@gmail.com>
2016-06-01 01:55:12 +0300

    dev: add details to routine screen

---
+++ app/components/routines/add/add-details/add-details.component.ts
@@ -0,0 +1,16 @@
+import {Component, Output} from '@angular/core';
+
+@Component({
+    templateUrl:        'app/components/routines/add/add-details/add-details.component.html',
+    styleUrls:          ['app/components/routines/add/add-details/add-details.component.css'],
+    providers:          []
+})
+
+export class AddDetailsComponent {
+    
+    constructor() { }
+    
+    ngOnInit() {
+        
+    }
+}
---
+++ app/components/routines/add/add-exercises/add-exercises.component.ts
@@ -0,0 +1,16 @@
+import {Component, Output} from '@angular/core';
+
+@Component({
+    templateUrl:        'app/components/routines/add/add-exercises/add-exercises.component.html',
+    styleUrls:          ['app/components/routines/add/add-exercises/add-exercises.component.css'],
+    providers:          []
+})
+
+export class AddExercisesComponent {
+    
+    constructor() { }
+    
+    ngOnInit() {
+        
+    }
+}
