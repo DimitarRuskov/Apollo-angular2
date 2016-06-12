9dbef1b05211bef99cc28d244333fa8898b03345
DimitarRuskov <dimi7ar.ru5kov@gmail.com>
2016-06-12 04:09:15 +0300

    dev: add category component update

--- app/components/categories/category/routines/add/add-details/add-details.component.ts
+++ app/components/categories/category/routines/add/add-details/add-details.component.ts
@@ -1,4 +1,4 @@
-import {Component, Output} from '@angular/core';
+import {Component, Output, ElementRef} from '@angular/core';
 
 @Component({
     moduleId: module.id,
@@ -8,10 +8,33 @@ import {Component, Output} from '@angular/core';
 })
 
 export class AddDetailsComponent {
+    private _selectedImage: any = null;
     
-    constructor() { }
+    constructor(private element: ElementRef) { }
     
     ngOnInit() {
         
+    }
+
+    onImageClick(event: any) {
+        let imagePicker = this.element.nativeElement.querySelector('.image-picker');
+        imagePicker.click();
+    }
+    
+    changeListener(event: any) {
+        let reader = new FileReader();
+        let image = this.element.nativeElement.querySelector('.image');
+
+        reader.onload = function(e: any) {
+            let src = e.target.result;
+            this._selectedImage = src;
+            image.src = src;
+        }.bind(this);
+
+        reader.readAsDataURL(event.target.files[0]);
+    }
+    
+    onSubmit(values: any): void {
+        debugger;
     }
 }
